cfe342e727bc674a809dadda3f9c6964fe210901
akhilgit19 <138980055+akhilgit19@users.noreply.github.com>
2024-09-10 23:04:09 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ sudo chmod 777 build
 current_date=$(date +%d%m%Y)
 aws s3api put-object --bucket testuploadbucket0 --key "${current_date}/"
 aws s3 cp --recursive build "s3://testuploadbucket0/${current_date}/$(basename build)"
-sudo docker build -t react-nginx:$git_commit -f golddockerfile .
+sudo docker build -t react-nginx:$git_commit -f golddockerfile1 .
 sudo docker tag react-nginx:$git_commit akhilpagadapoola/react-nginx:$git_commit ##make sure you did docker login
 sudo docker push akhilpagadapoola/react-nginx:$git_commit
 aws s3 rm s3://gitcommitids/new_value.txt
